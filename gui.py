1cbb495d5e9a1c1a3554b7fe572fb91eb5895f5d
Junior R. Ribeiro-Akai <j5r@outlook.com>
2020-03-09 19:10:44 -0300

    editando a gui2 (2a versao)

--- gui.py
+++ gui.py
@@ -9,6 +9,7 @@ class Gui(Tk):
         self.option_add('*tearOff', FALSE)
         self.geometry("500x300")
         self.title("Lattes2Excel")
+        self.fonte = ("Arial",14,"bold")
 
         if os.sys.platform == "linux":
             self.__comando = "xgd-open "
@@ -18,6 +19,7 @@ class Gui(Tk):
 
         self.links = []
         self.nome_arquivo = "unknown.txt"
+        self.links_as_texto = ""
 
         barra_de_menus = Menu(self)
         self['menu'] = barra_de_menus
@@ -29,10 +31,19 @@ class Gui(Tk):
         barra_de_menus.add_cascade(menu=menu_ajuda, label='Ajuda')
 
         menu_principal.add_command(label='Abrir...', command=self.abrir_arquivo)
-        menu_principal.add_command(label='Novo Arquivo', command=None)
+        menu_principal.add_command(label='Novo Arquivo', command=self.novo_arquivo)
 
         menu_ajuda.add_command(label='Como usar', command=None)
         menu_ajuda.add_command(label='Sobre Lattes2Excel', command=None)
+
+        self.lnome_arquivo = Label(self,text="Arquivo",width=15)
+        self.lnome_arquivo.pack(side=RIGHT)
+        self.lnnome_arquivo = Label(self,text=self.nome_arquivo)
+        self.lnnome_arquivo.pack()
+        self.lnnome_arquivo["fg"] = "#f00"
+        self.lnnome_arquivo["bg"] = "#fdd"
+
+        self.text_area = Text(self, width=55,height=12)
 
 
         self.mainloop()
@@ -42,10 +53,20 @@ class Gui(Tk):
         self.nome_arquivo = arquivo.split("/")[-1]
         f = open(arquivo,"r")
         self.links = list(map(lambda l: l.strip(),f.readlines()))
+        self.links_as_texto = "\n".join(self.links)
         f.close()
-        print(self.links)
+        self.novo_arquivo()
+        self.lnnome_arquivo["text"] = self.nome_arquivo
+        print(self.links_as_texto)
 
     def abrir_link(self, link):
         os.system(self.__comando + link)
+
+    def novo_arquivo(self):
+        self.text_area.pack()
+        self.text_area.insert(0.0,self.links_as_texto)
+
+        self.salvar_avancar = Button(self,)
+
 
 Gui()
